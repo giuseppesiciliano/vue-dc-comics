ead1d9bcfc26c1f01ec81773fb01ce8f51624802
giuseppesiciliano <giuseppesiciliano90@gmail.com>
2021-12-20 16:17:56 +0100

    HEADER done

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,19 @@
 <template>
     <header>
         <div class="container">
-            <h1>HEADER</h1>
+            <div class="content">
+                <img class="logo" src="../assets/img/dc-logo.png" alt="DC Logo">
+
+                <!-- Menu -->
+                <nav>
+                    <ul>
+                        <li v-for="(link, index) in links" :key="index" :class="{'currentLink': index === currentLink}">
+                            <a :href="link.url">{{ link.text }}</a>
+                        </li>
+                    </ul>
+                </nav>
+                
+            </div>
         </div>
     </header>
 </template>
@@ -9,13 +21,112 @@
 
 <script>
 export default {
-    name: 'Header'
+    name: 'Header',
+    data: function() {
+        return {
+            currentLink: 1,
+            links: [
+                {
+                    text: 'CHARACTERS',
+                    url: '#',
+                    current: 'false'
+                },
+                {
+                    text: 'COMICS',
+                    url: '#',
+                    current: 'false'
+                },
+                {
+                    text: 'MOVIES',
+                    url: '#',
+                    current: 'false'
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    current: 'false'
+                },
+                {
+                    text: 'GAMES',
+                    url: '#',
+                    current: 'false'
+                },
+                {
+                    text: 'COLLECTIBLES',
+                    url: '#',
+                    current: 'false'
+                },
+                {
+                    text: 'VIDEOS',
+                    url: '#',
+                    current: 'false'
+                },
+                {
+                    text: 'FANS',
+                    url: '#',
+                    current: 'false'
+                },
+                {
+                    text: 'NEWS',
+                    url: '#',
+                    current: 'false'
+                },
+                {
+                    text: 'SHOP',
+                    url: '#',
+                    current: 'false'
+                }
+            ]
+        }
+    }
 }
 </script>
 
 <style scoped lang="scss">
+@import '../style/variables.scss';
+
+
 header {
-    height: 100px;
-    background-color: pink;
+    height: 130px;
+    background-color: white;
+}
+
+.content {
+    line-height: 125px;
+}
+
+.logo {
+    position: relative;
+    top: 15px;
+    width: 100px;
+    float: left;
+}
+
+// Menu
+nav {
+    float: right;
+}
+ul {
+    display: flex;
+}
+li {
+    padding: 0 30px;
+
+    &.currentLink {
+        border-bottom: 5px solid $brand_primary_color;
+        a {
+            color: $brand_primary_color;
+        }
+    }
+}
+li:hover {
+    border-bottom: 5px solid $brand_primary_color;
+    a {
+        color: $brand_primary_color;
+    }
+}
+a {
+    color: black;
+    font-weight: bold;
 }
 </style>

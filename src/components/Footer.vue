40eef9de67ca03117922759e4c6dbd7399fad306
giuseppesiciliano <giuseppesiciliano90@gmail.com>
2021-12-20 17:54:34 +0100

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,157 @@
 <template>
     <footer>
-        <div class="bg-image"></div>
-        <div class="container"></div>
+        <div class="bg-image">
+
+            <div class="container">
+                <div class="list">
+
+                    <!-- COL-LEFT -->
+                    <div class="col left">
+
+                        <!-- DC Comics -->
+                        <div class="list-name">
+                            <div class="text">DC COMICS</div>
+                            <ul>
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <!-- Shop -->
+                        <div class="list-name">
+                            <div class="text">SHOP</div>
+                            <ul>
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- END COL-LEFT -->
+
+
+                    <!-- COL-CENTRAL -->
+                    <div class="col central">
+
+                        <!-- DC -->
+                        <div class="list-name">
+                            <div class="text">DC</div>
+                            <ul>
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- END COL-CENTRAL -->
+
+
+                    <!-- COL-RIGHT -->
+                    <div class="col right">
+
+                        <!-- DC -->
+                        <div class="list-name">
+                            <div class="text">SITES</div>
+                            <ul>
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- END COL-RIGHT -->
+                </div>
+            </div>
+        </div>
+
+        
+
         <div class="sign-up"></div>
     </footer>
 </template>
@@ -9,17 +159,167 @@
 
 <script>
 export default {
-    name: 'Footer'
+    name: 'Footer',
+    data: function() {
+        return {
+            lists: [
+                {
+                    title: 'DC COMICS',
+                    links: [
+                        {
+                            name: 'Characters',
+                            url: '#'
+                        },
+                        {
+                            name: 'Comics',
+                            url: '#'
+                        },
+                        {
+                            name: 'Movies',
+                            url: '#'
+                        },
+                        {
+                            name: 'TV',
+                            url: '#'
+                        },
+                        {
+                            name: 'Games',
+                            url: '#'
+                        },
+                        {
+                            name: 'Videos',
+                            url: '#'
+                        },
+                        {
+                            name: 'News',
+                            url: '#'
+                        }
+                    ]
+                },
+                {
+                    title: 'SHOP',
+                    links: [
+                        {
+                            name: 'Shop DC',
+                            url: '#'
+                        },
+                        {
+                            name: 'Schop DC Collectibles',
+                            url: '#'
+                        }
+                    ]
+                },
+                {
+                    title: 'DC',
+                    links: [
+                        {
+                            name: 'Terms Of Use',
+                            url: '#'
+                        },
+                        {
+                            name: 'Privacy policy (New)',
+                            url: '#'
+                        },
+                        {
+                            name: 'Ad Choises',
+                            url: '#'
+                        },
+                        {
+                            name: 'Advertising',
+                            url: '#'
+                        },
+                        {
+                            name: 'Jobs',
+                            url: '#'
+                        },
+                        {
+                            name: 'Subscriptions',
+                            url: '#'
+                        },
+                        {
+                            name: 'Talent Workshops',
+                            url: '#'
+                        },
+                        {
+                            name: 'CPSC Certificates',
+                            url: '#'
+                        },
+                        {
+                            name: 'Ratings',
+                            url: '#'
+                        },
+                        {
+                            name: 'Shop Help',
+                            url: '#'
+                        },
+                        {
+                            name: 'Contact Us',
+                            url: '#'
+                        }
+                    ]
+                },
+                {
+                    title: 'SITES',
+                    links: [
+                        {
+                            name: 'DC',
+                            url: '#'
+                        },
+                        {
+                            name: 'MAD Magazine',
+                            url: '#'
+                        },
+                        {
+                            name: 'DC Kids',
+                            url: '#'
+                        },
+                        {
+                            name: 'DC Universe',
+                            url: '#'
+                        },
+                        {
+                            name: 'DC Power Visa',
+                            url: '#'
+                        }
+                    ]
+                }
+            ]
+        }
+    }
 }
 </script>
 
 
 <style scoped lang="scss">
+@import '../style/common.scss';
+
+
 .bg-image {
     background-image: url("../assets/img/footer-bg.jpg");
     height: 500px;
     background-repeat: no-repeat;
     background-size: cover;
+}
+
+.list {
+    display: flex;
+    padding: 50px;
+    color: white;
+}
+.col {
+    padding-right: 50px;
+}
+.list-name {
+    padding-bottom: 30px;
+
+    li {
+        padding: 5px 0;
+        color: gray;
+
+        a {
+            color: gray;
+        }
+    }
 }
 
 .sign-up {
